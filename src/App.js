2475bb51bacec32c4bdb397b943bd4c730d3917b
Benjamin Dordoigne <ben.dordoigne@gmail.com>
2019-08-28 17:48:16 +0100

    Added Search

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Fragment, Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
-import Users from './components/users/Search'
+import Search from './components/users/Search'
 
 import axios from 'axios';
 import './App.css';
+
 
 class App extends Component{
 
@@ -13,16 +14,25 @@ class App extends Component{
     loading: false
   }
   
-  async componentDidMount() {
+  // async componentDidMount() {
+  //   this.setState({ loading: true });
+  //     const res = await axios.get(
+  //       `https://api.github.com/users?client_id=${
+  //         process.env.REACT_APP_GITHUB_CLIENT_ID
+  //       }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+  //   this.setState({users: res.data, loading: false});
+  // }
+   
+  searchUsers = async text => {
+    const res = await axios.get(
+      `https://api.github.com/users?q${text}&client_id=${
+        process.env.REACT_APP_GITHUB_CLIENT_ID
+      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+  
+    this.setState({ users: res.data.items, loading: false });  
+    }
 
-    console.log(process.env.REACT_APP_GITHUB_CLIENT_SECRET);
 
-    this.setState({ loading: true });
-
-    const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-
-    this.setState({users: res.data, loading: false});
-  }
   
   render(){
       return (
@@ -30,7 +40,7 @@ class App extends Component{
           <Navbar title="GitHub Finder" icon="fab fa-github"/>
           
           <div className="container">
-            <Search />
+            <Search searchUsers={this.searchUsers} /> 
             <Users loading={this.state.loading} users={this.state.users}/>          
           </div>
         </Fragment>
