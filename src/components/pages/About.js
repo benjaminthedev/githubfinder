16551ac7273e115c523357d08c73fb67ff1ddeae
Benjamin Dordoigne <ben.dordoigne@gmail.com>
2019-09-01 14:56:46 +0100

    Added React Router and likns

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React, {Fragment} from 'react'
+
+
+const About = () => {
+    return (
+        <Fragment>
+            <h1>About Page</h1>
+            <em>Search GitHub  Users</em>
+        </Fragment>
+    )
+}
+
+export default About
